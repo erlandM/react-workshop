140e716a6394cd449f20e2caae1dc7b9c9f75838
erlandM <erland.96@hotmail.com>
2026-03-04 09:53:18 +0000

    easy task 5 done

--- docs/1_introduction/assignment/easy/05_destructuring.ts
+++ docs/1_introduction/assignment/easy/05_destructuring.ts
@@ -9,22 +9,24 @@ type User = { name?: string; address?: { city?: string } };
 export function userSummary(u: User): string {
   // TODO:
   // - hent name med default "Ukjent"
+  const { name ="Ukjent"}= u;
   // - hent city fra address med default "Ukjent by"
+  const { address: { city = "Ukjent by" } ={}}=u;
   // - bruk sikre defaults på mellomledd (={} / ?? {})
-  return '';
+  return `${name} @ ${city}`;
 }
 
 // 2) Array-destructuring: hent ut brukernavnet
 export function getUsername(user: [string, string, string]): string {
   // TODO
-  return "";
+  const [id, username, link] = user;
+  return username;
 }
 
 // 3) Destructuring i parameter + rest
 export function pickIdAndRest(item: { id: number; [k: string]: unknown }) {
   // TODO: plukk ut id, legg resten i 'rest'
-  const id = 0;
-  const rest = {};
+  const { id, ...rest } = item;
   return { id, rest };
 }
 
@@ -34,9 +36,9 @@ export function pickIdAndRest(item: { id: number; [k: string]: unknown }) {
  *  ------------------------------------------------------------------
 */
 
-console.log(`Answer: ${userSummary({})}\t\t\tExpected: Ukjent @ Ukjent by`);
+console.log(`Answer: ${userSummary({})}\t\tExpected: Ukjent @ Ukjent by`);
 console.log(`Answer: ${userSummary({ name: 'Ada', address: { city: 'Bergen' } })}\t\t\tExpected: Ada @ Bergen`);
 
-console.log(`Answer: ${getUsername(["12", "pålKåre", "/link"])}\t\t\tExpected: pålKåre`);
+console.log(`Answer: ${getUsername(["12", "pålKåre", "/link"])}\t\t\t\tExpected: pålKåre`);
 
-console.log(`Answer: ${JSON.stringify(pickIdAndRest({ id: 7, a: 1, b: 2 }))}\t\t\tExpected: { "id":7, "rest":{ "a":1, "b":2 }`);
+console.log(`Answer: ${JSON.stringify(pickIdAndRest({ id: 7, a: 1, b: 2 }))}\tExpected: { "id":7, "rest":{ "a":1, "b":2 }`);
