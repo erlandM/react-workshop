1f475c7aaa445c66b905b3a8f85afcfd64eef333
erlandM <erland.96@hotmail.com>
2026-03-04 09:17:20 +0000

    easy task 4 done

--- docs/1_introduction/assignment/easy/04_functions_arrows.ts
+++ docs/1_introduction/assignment/easy/04_functions_arrows.ts
@@ -5,16 +5,26 @@
  */
 
 // 1) Skriv skriv om funksjonene slik at den blir en arrow function
-export function add(a: number, b: number): number {
+export function addOld(a: number, b: number): number {
+  return a+b;
+}
+const add = (a:number, b:number): number => {
   return a+b;
 }
 
 // 2) Gjør skriv om denne arrow-funksjonen til en vanlig function og fiks logikken slik at den gir tibake riktig svar
-export const evenOdd = (n: number): 'even' | 'odd' => {
+export const evenOddOld = (n: number): 'even' | 'odd' => {
   // tips: du kan sjekke om et tall er delbart på 2 med n % 2 === 0
   return 'even';
 };
-
+export function evenOdd (n:number): 'even'|'odd' {
+  if (n % 2 === 0) {
+    return 'even';
+  }
+  else {
+    return 'odd';
+  }    
+}
 
 /** -------------------------- Self-check ---------------------------- 
  *  Kjør følgende kommando for å se om koden din kjørte
@@ -23,4 +33,4 @@ export const evenOdd = (n: number): 'even' | 'odd' => {
 */
 
 console.log(`Answer: ${add(2, 3)}\t\t\tExpected: 5`);
-console.log(`Answer: ${evenOdd(2), evenOdd(3)}\t\t\tExpected: even odd`);
+console.log(`Answer: ${evenOdd(2)} ${evenOdd(3)}\t\tExpected: even odd`);
