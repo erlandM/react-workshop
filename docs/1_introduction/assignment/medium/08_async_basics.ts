f6c3435cc4fc7024deec59000d3a874a323ec343
erlandM <erland.96@hotmail.com>
2026-03-09 11:20:56 +0000

    task 08 done

--- docs/1_introduction/assignment/medium/08_async_basics.ts
+++ docs/1_introduction/assignment/medium/08_async_basics.ts
@@ -9,36 +9,52 @@ export const sleep = (ms: number) => new Promise<void>(r => setTimeout(r, ms));
 // 2) fetchValue(label, ms): vent ms og returner label
 export async function fetchValue(label: string, ms: number): Promise<string> {
   // TODO: bruk sleep, returner label etterpå
-  return '';
+  await sleep(ms);
+  return label;
 }
 
 // 3) seqAB: hent "A"(300ms) og deretter "B"(300ms) SEKVENSIELT (ca. 600ms)
 export async function seqAB(): Promise<string[]> {
   // TODO: await fetchValue('A',300); så await 'B'
-  return [];
+  const a = await fetchValue('A', 300);
+  const b = await fetchValue('B', 300);
+  return [a, b];
 }
 
 // 4) parAB: hent "A" og "B" PARALLELT (ca. 300ms) med Promise.all
 export async function parAB(): Promise<string[]> {
   // TODO
-  return [];
+  const [a, b] = await Promise.all([fetchValue('A', 300), fetchValue('B', 300)]);
+  return [a, b];
 }
 
 // 5) sometimesFails(shouldFail): reject ved shouldFail=true
 export async function sometimesFails(label: string, shouldFail: boolean): Promise<string> {
   // TODO: hvis shouldFail -> throw Error(label), ellers returner label etter 100ms
-  return '';
+  if (shouldFail){
+    throw Error(label);
+  }
+  else{
+    await sleep(100)
+    return label
+  }
 }
 
 // 6) safeRun: kjør en async funksjon med try/catch og returner "ok:<value>" eller "error:<msg>"
 export async function safeRun(task: () => Promise<string>): Promise<string> {
   // TODO: try/catch
-  return '';
+  try {
+    return await task().then(value => `ok:${value}`);
+  }
+  catch (error) {
+    return await task().then(msg=> `error:${msg}`);
+  }
 }
 
 /** -------------------------- Self-check ----------------------------
  *  Kjør følgende kommando for å se om koden din kjørte
  *  npx tsx tasks/js-recap/medium/02_async_basics.ts
+ *  npx tsx docs/1_introduction/assignment/medium/08_async_basics.ts
  *  ------------------------------------------------------------------
 */
 (async () => {
