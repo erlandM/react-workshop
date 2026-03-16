03c880c84a814c1574454b7f6ad5a6fdd8fd4817
erlandM <erland.96@hotmail.com>
2026-03-16 13:43:41 +0000

    Started working on the header

--- src/components/features/Header.tsx
+++ src/components/features/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header id="example-header">
+    <header id="example-header" style={{ height: '50px', width: '100%' }}>
       <img src="/vite.svg" alt="site logo" />
       <nav>menu</nav>
     </header>
