f89797baa0c0b5590b336e6f5fa4df71dd06a440
erlandM <erland.96@hotmail.com>
2026-03-18 09:59:00 +0000

    Header done?

--- src/components/features/Header.tsx
+++ src/components/features/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
-    <header id="example-header" style={{ height: '50px', width: '100%' }}>
-      <img src="/vite.svg" alt="site logo" />
+    <header className="header">
+      <img src="src/assets/GoogleLogo.png" alt="site logo" />
       <nav>menu</nav>
     </header>
   );
